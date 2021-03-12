a39ad61a60a9190f56d37ce08b181f784ccba35c
Auties00 <alautiero@gmail.com>
2021-03-12 17:45:08 +0100

    Update

--- scripts/deploy_website.dart
+++ scripts/deploy_website.dart
@@ -12,25 +12,22 @@ void main() {
   print('Build output:${buildResult.stdout}\nBuild errors:${buildResult.stderr}\n');
 
   print("Cleaning last build...");
-  var git = Directory(gitPath);
-  for (var entry in git.listSync()) {
-    if (!entry.path.endsWith('.git')) {
-      entry.deleteSync(recursive: true);
-    }
-  }
+  Directory(gitPath).listSync()
+      .where((element) => !element.path.endsWith(".git"))
+      .forEach((element) => element.deleteSync(recursive: true))) {
 
   print("Copying new build...");
   var web = Directory(webPath);
   for (var entry in web.listSync(recursive: true)) {
-    if (FileSystemEntity.typeSync(entry.path) == FileSystemEntityType.file) {
-      File(entry.path.replaceAll(webPath, gitPath))
-        ..createSync(recursive: true)
-        ..writeAsBytesSync(File(entry.path).readAsBytesSync());
-    }
+  if (FileSystemEntity.typeSync(entry.path) == FileSystemEntityType.file) {
+  File(entry.path.replaceAll(webPath, gitPath))
+  ..createSync(recursive: true)
+  ..writeAsBytesSync(File(entry.path).readAsBytesSync());
+  }
   }
 
   print("Applying changes for github pages...");
   var result = Process.runSync(upgradeSitePath, []);
   print('Build output:${result.stdout}\nBuild errors:${result.stderr}');
   print("Process completed with exit code ${result.exitCode}");
-}
+  }
